810ca81b3ba7bf926b672f54725f2e852bd0cf3e
gabstxol <gabriel.programador014@gmail.com>
2025-02-14 12:19:17 -0300

    Gabriel Oliveira|Portifólio

---
+++ script.js
@@ -0,0 +1,12 @@
+var nike = document.getElementById("nike");
+nike.addEventListener('click',()=>{
+    window.open("https://gabstxol.github.io/nike/")
+})
+var americanas = document.getElementById("americanas");
+americanas.addEventListener('click',()=>{
+    window.open("https://gabstxol.github.io/americanas/")
+})
+var avengers = document.getElementById("avengers");
+avengers.addEventListener('click',()=>{
+    window.open("https://gabstxol.github.io/1--desafio/")
+})
